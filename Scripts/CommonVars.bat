3b4ac24325f6e9391e40232eedf922fd6cdef491
Csaba Pinter <pinter.csaba@gmail.com>
2022-06-05 15:20:01 +0100

    DOC: Update scripts

--- Scripts/CommonVars.bat
+++ Scripts/CommonVars.bat
@@ -5,22 +5,19 @@ set DEVEL_DIR=e:\t
 REM Directory where this file is located
 set BUILD_SCRIPT_DIR=%~dp0
 
-SET SVN_EXE="c:\Program Files\SlikSvn\bin\svn.exe"
-SET SVNVERSION_EXE="c:\Program Files\SlikSvn\bin\svnversion.exe"
 set GIT_EXE="c:\Program Files\Git\bin\git.exe"
 set PATCH_EXE="c:\Program Files\Git\usr\bin\patch.exe"
 set CMAKE_EXE="c:\Program Files\CMake\bin\cmake.exe"
 set CPACK_EXE="c:\Program Files\CMake\bin\cpack.exe"
 set CTEST_EXE="c:\Program Files\CMake\bin\ctest.exe"
 
-set DOT_EXE="C:/Program Files (x86)/Graphviz2.38/bin/dot.exe"
 set DOXYGEN_EXE="C:/Program Files/doxygen/bin/doxygen.exe"
 
-set QT5_DIR_X64=C:/Qt/5.15.1/5.15.1/msvc2019_64/lib/cmake/Qt5
+set QT5_DIR_X64=C:/Qt/5.15.2/5.15.2/msvc2019_64/lib/cmake/Qt5
 
-set CMAKE_GENERATOR_X64="Visual Studio 16 2019"
+set CMAKE_GENERATOR_X64="Visual Studio 17 2022"
 
-set MSBUILD_EXE="c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe"
+set MSBUILD_EXE="c:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
 
 set TRAINUS_SOURCE_DIR=%DEVEL_DIR%\TrainUS
 set TRAINUS_SUPERBUILD_BIN_DIR_DBG_X64=%DEVEL_DIR%\tD
